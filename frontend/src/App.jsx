94e5d818d1d2ce85ca0dbd027c920930bf461083
Prasada432 <tej9205@gmail.com>
2025-08-05 18:57:58 +0530

    Initial Backend Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center text-3xl font-bold p-10">
+      Chronoscribe Frontend Working!
+    </div>
+  );
+}
+
+export default App;
